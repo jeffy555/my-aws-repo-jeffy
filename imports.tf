62bfa75bf2e910c4f548b0e38c70ce14cc6dcf01
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-11 02:25:22 +0530

    feat: update infrastructure with new Azure resources and configurations

---
+++ imports.tf
@@ -0,0 +1,39 @@
+import {
+  to = azurerm_resource_group.migrate_scope
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg"
+}
+
+import {
+  to = azurerm_service_plan.asp_myresumeliverg_ade0
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg/providers/Microsoft.Web/serverFarms/ASP-myresumeliverg-ade0"
+}
+
+import {
+  to = azurerm_virtual_network.myresume_live_rg_vnet
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg/providers/Microsoft.Network/virtualNetworks/myresume-live-rg-vnet"
+}
+
+import {
+  to = azurerm_storage_account.preacherjefferson
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg/providers/Microsoft.Storage/storageAccounts/preacherjefferson"
+}
+
+import {
+  to = azurerm_application_insights.jeffersonimmanuel
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg/providers/Microsoft.Insights/components/jeffersonimmanuel"
+}
+
+import {
+  to = azurerm_monitor_action_group.application_insights_smart_detection
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg/providers/Microsoft.Insights/actionGroups/Application Insights Smart Detection"
+}
+
+import {
+  to = azurerm_monitor_action_group.recommended_alert_rules_ag_6b460f
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg/providers/Microsoft.Insights/actionGroups/RecommendedAlertRules-AG-6b460f"
+}
+
+import {
+  to = azurerm_linux_web_app.jeffersonimmanuel
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg/providers/Microsoft.Web/sites/jeffersonimmanuel"
+}
